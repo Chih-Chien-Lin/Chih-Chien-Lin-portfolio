a0ff3be956a8af70b73a2dce45de002c2f9760cb
Justin Lin <ro0566320@gmail.com>
2020-05-14 11:57:22 -0400

    make icons working

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -11,13 +11,20 @@ import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
+import FolderIcon from '@material-ui/icons/Folder';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
+import photo from '../asset/image/personal_photo.JPG'
+import { AiFillGithub, AiFillLinkedin, AiFillHtml5 } from 'react-icons/ai';
+import { IoLogoCss3, IoLogoJavascript, IoLogoNodejs } from 'react-icons/io'
+import { FaReact, FaNpm } from 'react-icons/fa'
+import { DiMysql, DiMongodb } from 'react-icons/di'
+
 
 const drawerWidth = 240;
 
@@ -118,7 +125,7 @@ function MainPage() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap>
-            Mini variant drawer
+
           </Typography>
         </Toolbar>
       </AppBar>
@@ -141,49 +148,52 @@ function MainPage() {
           </IconButton>
         </div>
         <Divider />
+
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
+          <ListItem button key={"Information"} >
+            <ListItemIcon><AssignmentIndIcon /> </ListItemIcon>
+            <ListItemText primary={"Information"} />
+          </ListItem>
+          <ListItem button key={"Project"}>
+            <ListItemIcon><FolderIcon /> </ListItemIcon>
+            <ListItemText primary={"Project"} />
+          </ListItem>
+          <ListItem button key={"Contact"}>
+            <ListItemIcon><MailIcon /> </ListItemIcon>
+            <ListItemText primary={"Contact"} />
+          </ListItem>
+
         </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
+
       </Drawer>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <div className="row"style={{marginTop:"8%", marginBottom:"10%"}}>
+          <div className="col-md-3">
+            <img src={photo} style={{ height: 300, width: 200, marginLeft:"20%"}}></img>
+          </div>
+          <div className="col-md-8">
+            <Typography paragraph>
+            <h2>Chih-Chien Lin</h2>  
+            <p>7 Primrose La, Colts Neck, NJ 07722 - (732)8043047 - <a class="text-lowercase" href="mailto:chihchienlin93@gmail.com">chihchienlin93@gmail.com</a></p>
+            <p>I am a full Stack Web Developer / Mechanical Engineer, passionate to programming skills and problem-solving. I love to learn and practice new technologies by working with other programmers to build cool projects! </p>
+            <br></br>
+            <h3>Skills</h3>
+            <p>The following contens are my projects and skills </p>
+            </Typography>
+            <br></br>
+            <a href="https://github.com/Chih-Chien-Lin"><i><AiFillGithub size={50} /></i></a> 
+            <a href="https://www.linkedin.com/in/chih-chien-justin-lin-691016ba/"><i><AiFillLinkedin size={50} style={{marginLeft:"5%"}} /></i></a>
+          </div>
+        </div>
+        <hr></hr>
+        <div style={{marginTop: "10%",marginBottom: "10%"}}>
+          <p >Skills</p>
+          <br></br><br></br>
+          <p>Programming Languages & Tools</p>
+          <br></br>
+
+        </div>
       </main>
     </div>
   );
